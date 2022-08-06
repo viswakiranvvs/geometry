15f8775c86164776be67d3d866be1c72cb7e1f6f
viswakiranvvs <viswachintu425@gmail.com>
2022-08-06 11:02:02 +0530

    initial commit

---
+++ lib/geometry.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "geometry/version"
+
+module Geometry
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/geometry/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Geometry
+  VERSION = "0.1.0"
+end
